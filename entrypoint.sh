3b8b49308ee9d69801624cc2575db6ea19f93de2
g-roll <83816302+g-roll@users.noreply.github.com>
2025-02-24 16:55:17 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Sleeping for 30 seconds..."
-sleep 30
+echo "Sleeping for 0 seconds..."
+sleep 10
+
+wp core download --locale=de_DE --skip-content
 
 # Install WordPress core if not already installed
 if ! wp core is-installed; then
