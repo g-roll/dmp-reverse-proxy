00d0155255ff5b905bac78a7c4e0b9342c3f4685
g-roll <83816302+g-roll@users.noreply.github.com>
2025-02-24 15:54:13 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Sleeping for 10 seconds..."
+sleep 10
+
 # Install WordPress core if not already installed
 if ! wp core is-installed; then
     wp core install --url="${WORDPRESS_URL}" \
