0863547929ab0d4d8eb3873192178cb07c8512ab
g-roll <83816302+g-roll@users.noreply.github.com>
2025-02-24 17:19:24 +0100

    wip

--- init.sh
+++ init.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "sleep 10 seconds"
+# Warte auf MySQL
 sleep 10
 
-wp config set FORCE_SSL_ADMIN true --raw
-wp config set WP_HOME "https://${DOMAIN}"
-wp config set WP_SITEURL "https://${DOMAIN}"
-wp config set 'WORDPRESS_CONFIG_EXTRA' '$_SERVER["HTTPS"] = "on"; $_SERVER["SERVER_PORT"] = 443;' --raw
+# Installiere wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+mkdir -p /var/www/.wp-cli
 
+# Ab hier läuft alles als www-data
+# wp config set FORCE_SSL_ADMIN true --raw
+# wp config set WP_HOME "https://${DOMAIN}"
+# wp config set WP_SITEURL "https://${DOMAIN}"
+# wp config set 'WORDPRESS_CONFIG_EXTRA' '$_SERVER["HTTPS"] = "on"; $_SERVER["SERVER_PORT"] = 443;' --raw
+
+# Apache startet automatisch als www-data
 exec apache2-foreground
