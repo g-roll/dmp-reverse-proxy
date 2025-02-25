edf668fe46f4c00a70d5df800ffdd96324b69ce1
g-roll <83816302+g-roll@users.noreply.github.com>
2025-02-25 16:30:36 +0100

    wip

--- create-dotenv.sh
+++ create-dotenv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Generate two random 20-character alphanumeric passwords (lowercase only)
+# Generate two random 20-character lowercase alphanumeric passwords
 MYSQL_PW=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 20 | head -n 1)
 MYSQL_ROOT_PW=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 20 | head -n 1)
 
-# Create/overwrite .env file with the passwords
 cat >.env <<EOF
 MYSQL_PASSWORD=$MYSQL_PW
 MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PW
