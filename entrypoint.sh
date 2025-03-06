cfe6762ccf72b2285e157030b16f192139225be3
g-roll <83816302+g-roll@users.noreply.github.com>
2025-03-06 16:58:37 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,6 @@ if ! wp core is-installed; then
     fi
 fi
 
-tc qdisc add dev eth0 root tbf rate 100mbit burst 100kb latency 50ms
+tc qdisc add dev eth0 root tbf rate 4mbit burst 32kb latency 50ms
 
 exec "$@"
