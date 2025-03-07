c29b36f5ffd4b2f5cf899b1ca061587739bc2a7c
g-roll <83816302+g-roll@users.noreply.github.com>
2025-03-07 15:26:33 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ if ! wp core is-installed; then
         --admin_password="dummy123" \
         --admin_email="admin@example.com"
 
-    wp plugin install --activate mainwp-child updraftplus all-in-one-wp-migration https://dmp-assets.b-cdn.net/wp/plugins/ai1wm.zip
+    wp plugin install --activate all-in-one-wp-migration https://dmp-assets.b-cdn.net/wp/plugins/ai1wm.zip
 
     cd /var/www/html/wp-content/ai1wm-backups/
 
@@ -24,6 +24,8 @@ if ! wp core is-installed; then
             exit 1
         fi
     fi
+
+    wp plugin install --activate mainwp-child updraftplus
 fi
 
 echo "Setting up network rate limit..."
